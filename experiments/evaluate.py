16fb952f0b285cbcbf839e5eb7ec6b5da543d571
Lukas Schmid <schmluk@mavt.ethz.ch>
2022-06-30 13:07:43 +0200

    fix and update evaluation pipeline

--- experiments/evaluate.py
+++ experiments/evaluate.py
@@ -1,91 +1,25 @@
-#!/usr/bin/env python
-from ast import parse
-from random import sample
-import time
 import datetime
-from planning.baseline_nbvp import LocalNBVPlanner
-from simulator.config import Config
-from simulator.simulator import Simulator
-from planning.baseline_planner import LocalSamplingPlanner
-from planning.policy_planner import PolicyPlanner
-from learning.model import MapEncoder, PoseEncoder, CnnGainEstimator, CNNGainEstimatorModel
-
 import pickle
 import math
 import os
 import numpy as np
 import torch
-import sys
-import argparse
-from matplotlib import pyplot as plt
-from matplotlib.colors import ListedColormap
 import yaml
+import sys
+
+from matplotlib import pyplot as plt
+
+from cvae_exploration_planning.planning.baseline_nbvp import LocalNBVPlanner
+from cvae_exploration_planning.simulator.config import Config
+from cvae_exploration_planning.simulator.simulator import Simulator
+from cvae_exploration_planning.planning.baseline_planner import LocalSamplingPlanner
+from cvae_exploration_planning.planning.policy_planner import PolicyPlanner
+# from cvae_exploration_planning.learning.model import CNNGainEstimatorModel, CVAE
 
 sys.path.append("../learning")
-sys.path.append("../experiments")
-sys.path.append('../simulator')
+# sys.path.append("../experiments")
+# sys.path.append('../simulator')
 torch.set_num_threads(1)
-
-
-def explore_worlds():
-    cfg = Config()  # sets up defaults
-    cfg.voxel_size = 0.2  # set custom params
-    cfg.initial_safety_margin = 2
-
-    # a little nicer plotting
-    free = np.array([1, 1, 1, 1])
-    occ = np.array([0, 0, 0, 1])
-    unknown = np.array([0.85, 0.9, 0.95, 1])
-    robot_cmp = ListedColormap(np.vstack((free, occ, unknown)))
-    world_cmp = ListedColormap(np.vstack((free, occ)))
-
-    if is_maze:
-        # Maze
-        cfg.world_type = cfg.world_type_maze
-        cfg.mz_n_walls = [100, 200]
-        cfg.mz_wall_strength = 0.5  # m
-        cfg.mz_wall_length = [30, 45]  # m
-        cfg.mz_wall_spacing = 5  # m
-    else:
-         # Cubes
-        cfg.world_type = cfg.world_type_cubes_and_spheres
-        cfg.cs_n_cubes = [10, 30]  # min, max
-        cfg.cs_n_spheres = [10, 30]
-        cfg.cs_cube_side_lengths = [1, 8]  # m
-        cfg.cs_spheres_radii = [1, 4]  # m
-
-    # setup the simulator
-    sim = Simulator(cfg)
-
-    # Execute planning steps.
-    while True:
-        plt.figure()
-        sim.reset_world()
-        sim.reset_robot()
-        world_map = sim.get_world_map()
-        x, y, yaw = sim.get_robot_pose()
-        np.savetxt(world_file + '.txt', np.array((x, y, yaw)))
-        plt.imshow(world_map, cmap=world_cmp)
-        plt.imshow(world_map, cmap=world_cmp, interpolation='none')
-        plt.arrow(y, x, math.sin(yaw) / cfg.voxel_size,
-                  math.cos(yaw) / cfg.voxel_size, width=0.3 / cfg.voxel_size,
-                  color='r')  # coordinate convention: x,y is in image coordinates, i.e. x = down, y = right
-
-        # plt.show()
-        plt.savefig(world_file + '.png')
-        sim.save_state_to_file(world_file)
-        observable = sim.get_explorable_area()
-        print("Displaying the generated world map and initial robot pose.\n"
-              "From this starting point, %.2f%s of the world (%i voxels) are observable."
-              % (observable / np.shape(world_map)[0] / np.shape(world_map)[
-            1] * 100,
-                 '%', observable))
-
-        # print("Enter '1' create a new world, or '2' to exit the demo:")
-        # k = input()
-        k = "2"
-        if k == "2":
-            break
 
 
 def save(obj, name):
@@ -107,38 +41,23 @@ def setup_sim(world_file, robot_pos=None):
     sim.load_state_from_file(world_file)
     sim._cfg.voxel_size = 0.2  # set custom params
     sim._cfg.initial_safety_margin = 2
-
-    if is_maze:
-        # World Maze
-        sim._cfg.world_type = Config.world_type_maze
-        sim._cfg.mz_n_walls = [100, 200]
-        sim._cfg.mz_wall_strength = 0.5  # m
-        sim._cfg.mz_wall_length = [30, 45]  # m
-        sim._cfg.mz_wall_spacing = 5  # m
-        sim._cfg.local_submap_size_x = 50
-        sim._cfg.local_submap_size_y = 50
-    else:
-        # World Cube and Sphere
-        sim._cfg.world_type = Config.world_type_cubes_and_spheres
-        sim._cfg.cs_n_cubes = [10, 30]  # min, max
-        sim._cfg.cs_n_spheres = [10, 30]
-        sim._cfg.cs_cube_side_lengths = [1, 8]  # m
-        sim._cfg.cs_spheres_radii = [1, 4]  # m
+    sim._cfg.local_submap_size_x = 50
+    sim._cfg.local_submap_size_y = 50
 
     if robot_pos:
         sim.robot.load_initial_position(robot_pos)
     return sim
 
 
-def run_planner(sim, n_samples, run_id, world_file, use_policy, policy_planner, local_planner, method='cvae', use_nbvp=False, localmap_size=50):
+def run_planner(sim, n_samples, run_id, output_dir, use_policy, policy_planner, local_planner, method='cvae', use_nbvp=False, time_limit=20):
     # Args
-    max_time = 20 * 60  # s
+    max_time = time_limit * 60  # s
     d_time = []
     d_expl = []
     d_log = []
     d_comp = []
     d_distance = []
-    d_global_plans = [] # None if was local, [reason, explored_after], 
+    d_global_plans = []  # None if was local, [reason, explored_after],
     current_time = 0
     global total_voxels
     total_voxels = sim.get_explorable_area()
@@ -147,7 +66,7 @@ def run_planner(sim, n_samples, run_id, world_file, use_policy, policy_planner, 
     while True:
         expl = sim.get_explored_area()
         print("Local Planning Step (t=%.1f, expl=%.1f%%)." % (
-                current_time, expl / total_voxels * 100))
+            current_time, expl / total_voxels * 100))
         d_time.append(current_time)
         d_expl.append(expl)
         global_plan = None
@@ -201,15 +120,14 @@ def run_planner(sim, n_samples, run_id, world_file, use_policy, policy_planner, 
 
     print("Finished exploration run")
     save({'time': d_time, 'expl': d_expl, 'comp': d_comp, 'log': d_log, 'global': d_global_plans, 'distance': d_distance},
-         world_file +'/sample%i_run%i_' % (n_samples, run_id) + method)
+         output_dir + '/sample%i_run%i_' % (n_samples, run_id) + method)
 
 
-def evaluate_multiple_planners(worlds, s, methods):
+def evaluate_multiple_planners(worlds, s, methods, output_dir, time_limit, n_runs):
     dt = 15
-    n_runs = 3
     continue_completed_runs = True
+    max_steps = int(time_limit * 60 / dt) + 1
 
-    max_steps = int(20 * 60 / dt) + 1
     means = {}
     std = {}
     for ii, method in enumerate(methods):
@@ -217,10 +135,11 @@ def evaluate_multiple_planners(worlds, s, methods):
         for j, world in enumerate(worlds):
             world_file = 'worlds/' + world
             sim = setup_sim(world_file)
-            total_voxels = sim.get_explorable_area() 
+            total_voxels = sim.get_explorable_area()
             for n in range(n_runs):
                 k = j * n_runs + n
-                d = load(world_file +'/sample%i_run%i_' % (s, n) + method)
+                d = load(
+                    f"{output_dir}/{world}/sample{s if method != 'regression' else 1}_run{n}_{method}")
                 time = d['time']
                 expl = d['expl']
 
@@ -244,24 +163,24 @@ def evaluate_multiple_planners(worlds, s, methods):
                             local_data[i, k] = np.nan
                     else:
                         local_data[i, k] = expl[current_index - 1] + (
-                                    expl[current_index] - expl[
+                            expl[current_index] - expl[
                                 current_index - 1]) * (t - time[
-                            current_index - 1]) / (time[current_index] - time[
-                            current_index - 1])
+                                    current_index - 1]) / (time[current_index] - time[
+                                        current_index - 1])
                     t = t + dt
                 local_data[:, k] /= total_voxels
-        # Get mean and std for uniform planner and learnt planner
+        # Get mean and std exploration
         means[ii] = np.nanmean(local_data, axis=1) * 100
         std[ii] = np.nanstd(local_data, axis=1) * 100
-        print(np.shape(means))
 
     # Plotting
     plt.figure()
-    colors = ['b', 'r', 'g', 'k', 'm', 'c', 'y']
-    styles = ['-', '-.', ':', 'dashed', 'dashdot', '--', 'dotted']
-    x = np.linspace(0, 20 * 60, max_steps) / 60
+    colors = ['b', 'r', 'g', 'k', 'm', 'c', 'y', 'orange', 'gold']
+    styles = ['-', '-.', ':', '--'] * 3
+    x = np.linspace(0, time_limit * 60, max_steps) / 60
     for i in range(len(methods)):
-        plt.plot(x, means[i], linestyle=styles[i], color=colors[i], label=methods[i])
+        plt.plot(x, means[i], linestyle=styles[i],
+                 color=colors[i], label=methods[i])
         plt.fill_between(x, means[i] - std[i],
                          means[i] + std[i],
                          facecolor=colors[i], alpha=.2)
@@ -271,43 +190,32 @@ def evaluate_multiple_planners(worlds, s, methods):
     plt.ylim(bottom=0)
     plt.xlim(left=0, right=x[-1])
     plt.legend()
-    dir = 'evaluation/plots/'
-    if not os.path.isdir(dir):
-        os.mkdir(dir)
-    name = dir + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "_n_samples_" + str(s)
     plt.draw()
-    plt.pause(2)
-    plt.savefig(name + ".png")
-
-
-def plot(worlds, n_samples, methods):
-    for n_sample in n_samples:
-        evaluate_multiple_planners(worlds, n_sample, methods)
+    plt.pause(0.01)
+    plt.savefig(f"{output_dir}/preformance_{s}_samples.png")
 
 
 def setup_local_planner(sim, n_sample, use_nbvp, localmap_size=50):
     # Args
-    max_time = 20 * 60  # s
     sampling_steps = n_sample
     sampling_yaw = 1
     count_only_feasible_samples = True
     use_normalized_gain = True
-    gain_decay_rate = 0.5 # Gain decay rate for NBVP
-    maximum_edge_length = 1 # Max edge length for NBVP
-
-    cfg = sim.config
+    gain_decay_rate = 0.5  # Gain decay rate for NBVP
+    maximum_edge_length = 1  # Max edge length for NBVP
 
     # Setup local planner
     action_bounds = [localmap_size / 2, localmap_size / 2,
                      math.pi]
     local_planner = None
     if use_nbvp:
-        local_planner =LocalNBVPlanner(sim.robot, sampling_steps,action_bounds,gain_decay_rate, True, maximum_edge_length)
+        local_planner = LocalNBVPlanner(
+            sim.robot, sampling_steps, action_bounds, gain_decay_rate, True, maximum_edge_length)
     else:
         local_planner = LocalSamplingPlanner(sim.robot, sampling_steps,
-                                         sampling_yaw, action_bounds,
-                                         count_only_feasible_samples,
-                                         use_normalized_gain)
+                                             sampling_yaw, action_bounds,
+                                             count_only_feasible_samples,
+                                             use_normalized_gain)
     return local_planner
 
 
@@ -318,40 +226,44 @@ def setup_policy(sim, local_planner, paths, method, n_samples):
         model = torch.load(paths['cvae'], map_location=torch.device('cpu'))
         model.to(torch.device("cpu"))
         model.eval()
-        policy_planner = PolicyPlanner(sim, local_planner, model, method, n_samples)
+        policy_planner = PolicyPlanner(
+            sim, local_planner, model, method, n_samples)
     elif method == 'regression':
-        regression_baseline = torch.load(paths['regression'], map_location=torch.device('cpu'))
+        regression_baseline = torch.load(
+            paths['regression'], map_location=torch.device('cpu'))
         regression_baseline.to(torch.device('cpu'))
         regression_baseline.eval()
-        policy_planner = PolicyPlanner(sim, local_planner, regression_baseline, method, n_samples)
-    elif method == 'predict_gain':
-        model_with_gain = torch.load(paths['gain_predict'], map_location=torch.device('cpu'))
+        policy_planner = PolicyPlanner(
+            sim, local_planner, regression_baseline, method, n_samples)
+    elif method == 'cvae_joint':
+        model_with_gain = torch.load(
+            paths['cvae_joint'], map_location=torch.device('cpu'))
         model_with_gain.to(torch.device("cpu"))
         model_with_gain.eval()
-        policy_planner = PolicyPlanner(sim, local_planner, model_with_gain, method, n_samples)
-    elif method == 'twostage_predict_gain' or method == 'uniform_predict':
+        policy_planner = PolicyPlanner(
+            sim, local_planner, model_with_gain, method, n_samples)
+    elif method == 'twostage_predict_gain' or method == 'uniform_gain_predict':
         model = torch.load(paths['cvae'], map_location=torch.device('cpu'))
         model.to(torch.device("cpu"))
         model.eval()
-        gain_estimator = torch.load(paths['gain_estimator'], map_location=torch.device('cpu'))
+        gain_estimator = torch.load(
+            paths['gain_estimator'], map_location=torch.device('cpu'))
         gain_estimator.to(torch.device("cpu"))
         gain_estimator.eval()
-        policy_planner = PolicyPlanner(sim, local_planner, model, method, n_samples, gain_estimator)
+        policy_planner = PolicyPlanner(
+            sim, local_planner, model, method, n_samples, gain_estimator)
     elif method == 'cnn_gain_predict' or method == 'cnn_uniform_gain_predict':
         model = torch.load(paths['cvae'], map_location=torch.device('cpu'))
         model.to(torch.device("cpu"))
         model.eval()
-        cnn_gain_estimator = torch.load(paths['cnn_gain_estimator'], map_location=torch.device('cpu'))
+        cnn_gain_estimator = torch.load(
+            paths['cnn_gain_estimator'], map_location=torch.device('cpu'))
         cnn_gain_estimator.to(torch.device("cpu"))
         cnn_gain_estimator.eval()
-        # from keras.models import load_model
-        # map_encoder = load_model("map_encoder")
-        # pose_encoder = load_model("pose_encoder")
-        # gain_predictor = load_model("decoder")
-        # cnn_gain_estimator = [map_encoder, pose_encoder, gain_predictor]
-        policy_planner = PolicyPlanner(sim, local_planner, model, method, n_samples, cnn_gain_estimator)
+        policy_planner = PolicyPlanner(
+            sim, local_planner, model, method, n_samples, cnn_gain_estimator)
     else:
-        print("Policy Name Invalid")
+        print(f"Policy name '{method}' is invalid!")
     return policy_planner
 
 
@@ -359,51 +271,53 @@ def main():
     with open('config.yaml') as file:
         cfg = yaml.load(file, Loader=yaml.FullLoader)
 
-    # global world file
-    global is_maze
-    is_maze = cfg['maze']
+    # Setup files and dirs.
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    output_dir = f"results/{timestamp}"
+    os.makedirs(output_dir)
 
-    # randomly generate maps
-    if cfg['explore_world']:
-        for file in cfg['worlds']:
-            global world_file
-            world_file = 'worlds/' + file
-            if not os.path.isdir(world_file+'/'):
-                os.mkdir(world_file+'/')
-            explore_worlds()
+    # Run all experiments.
+    for world in cfg['worlds']:
+        print(f"===== Running on world '{world}' =====")
+        world_file = 'worlds/' + world
+        os.mkdir(os.path.join(output_dir, world))
+        if not os.path.isfile(world_file + '.p'):
+            print(f"Specified world file '{world_file}.p' does not exist.")
+            continue
+        if not os.path.isfile(world_file + '.txt'):
+            print(f"Specified world file '{world_file}.txt' does not exist.")
+            continue
 
-    # evaluate planners
-    n_runs = cfg['n_runs']
-    n_samples = cfg['n_samples']
-    if cfg['regression']: 
-        n_samples = [1]
-    
-    for file in cfg['worlds']:
-        print(file)
-        world_file = 'worlds/' + file
-        if not os.path.isdir(world_file+'/'):
-            os.mkdir(world_file+'/')
-        is_maze = cfg['maze']
-        is_maze = False if file[0] == 'c' else True
+        # Setup the simulation.
         pos = np.loadtxt(world_file + '.txt')
-        initial_position = [0,0,0]
-        initial_position[0], initial_position[1], initial_position[2] = int(pos[0]), int(pos[1]), pos[2]
-        print("evaluating map: ", file)
+        initial_position = [int(pos[0]), int(pos[1]), pos[2]]
         for method in cfg['planners']:
-            print("evaluating method: ", method)
+            n_samples = cfg['n_samples']
+            if method == "regression":
+                n_samples = [1]
+            print(f"----- Evaluating method '{method}' -----")
             use_policy = False if method == "uniform" or method == 'nbvp' else True
             use_nbvp = True if method == 'nbvp' else False
-            for run_id in range(n_runs):
+            for run_id in range(cfg['n_runs']):
                 for n_sample in n_samples:
-                    print("Running %i samples run %i" % (n_sample, run_id))
+                    print("Running %i samples, run number %i/%i." %
+                          (n_sample, run_id + 1, cfg['n_runs']))
                     sim = setup_sim(world_file, initial_position)
-                    local_planner = setup_local_planner(sim, n_sample, use_nbvp)
-                    policy = setup_policy(sim, local_planner, cfg['path'], method, n_sample)
-                    run_planner(sim, n_sample, run_id, world_file, use_policy, policy, local_planner, method, use_nbvp)
+                    local_planner = setup_local_planner(
+                        sim, n_sample, use_nbvp)
+                    policy = setup_policy(
+                        sim, local_planner, cfg['path'], method, n_sample) if use_policy else None
+                    run_planner(sim, n_sample, run_id, output_dir + '/' + world, use_policy,
+                                policy, local_planner, method, use_nbvp, cfg['time_limit'])
 
-    # plot
+    # Plotting of results
     if cfg['plot']:
-        plot(cfg['worlds'], cfg['n_samples'], cfg['planners'])
+        print("Plotting results.")
+        for n_sample in cfg['n_samples']:
+            evaluate_multiple_planners(
+                cfg['worlds'], n_sample, cfg['planners'], output_dir, cfg['time_limit'], cfg['n_runs'])
+
+    print("Evaluation compelted.")
 
 
 if __name__ == '__main__':
